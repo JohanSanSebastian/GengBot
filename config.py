ba5bbeada47d4abaf5b8bb4b9e8cd440573e33a4
Johan Sebastian <65955878+JohanSanSebastian@users.noreply.github.com>
2021-03-11 10:42:25 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-bot_token = "ODExOTg2MjEwNTEwNzMzMzkz.YC6LFg.80cnKc9NAZPHhjziPNjnpiLb8oE"
-join_role = "819449677295648779"
-wlcm_chnl = "774917896107327491"
-mod_chnl = "819445031081082910"
-poll_chnl = "813111739335180299"
+bot_token = 
+join_role = 
+wlcm_chnl = 
+mod_chnl = 
+poll_chnl = 
